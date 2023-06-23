f5f5ae68ba78ff3c02e88ccc7039006f54b300b0
neverbot <ivan@neverbot.com>
2023-06-23 16:04:28 +0200

    chore: add comment

--- src/dates.js
+++ src/dates.js
@@ -1,3 +1,11 @@
+// Date.prototype.getJulian = () => {
+//   return Math.floor(this / 86400000 - this.getTimezoneOffset() / 1440 + 2440587.5);
+// };
+
+// Date.prototype.oaDate = () => {
+//   return (this - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+// };
+
 const getJulian = (date) => {
   return Math.floor(date / 86400000 - date.getTimezoneOffset() / 1440 + 2440587.5);
 };
